1fa0861e1d070e727436c74bf46539de712e47cd
Chinmay Goyal <cg2chinmay@gmail.com>
2020-03-03 00:05:43 +0530

    Login Route

--- backend/app.js
+++ backend/app.js
@@ -3,20 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var app = express();
+var bodyParser = require('body-parser');
 
 var sql = require('./database.js');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
 
 // view engine setup
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('../database.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Yo' });
+});
+
+//route for login
+router.post('/',function(req,res,next){
+  var password = req.body.password;
+  var email = req.body.email;
+  connection.query("SELECT * FROM USERS where EMAIL=? AND PASSWORD=?",[email,password], function (err, result, fields) {
+    if (err) throw err;
+    if( result == '') console.log("Unauthorized access");
+    else console.log("Success");
+  });
+  res.send("Yo");
 });
 
 module.exports = router;
