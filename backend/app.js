924e1783e47c4a8235a13c9fb84c2645908d3343
sartkap <sarthakkapoor76@gmail.com>
2020-03-03 00:07:59 +0530

    User apis added

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,8 @@ var sql = require('./database.js');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var userRouter = require('./controllers/User');
+var app = express();
 
 // view engine setup
 app.use(bodyParser.json()); // support json encoded bodies
@@ -31,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/removeCookie',indexRouter);
 app.use('/users', usersRouter);
+app.use('/user',userRouter);
 app.use(session({secret: "chinmayg"}));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -1,5 +1,10 @@
 var mysql = require('mysql');
 var uuid = require('uuid');
+var express = require('express');
+var router = express.Router();
+// var http = require('http');
+var app = express();
+// var bodyParser = require('body-parser');
 var con = mysql.createConnection({
   host: 'localhost',
   user: 'newuser',
@@ -10,12 +15,33 @@ var con = mysql.createConnection({
 con.connect(function(err){
     if(err) throw err;
     console.log('Connected');
-    var sql = "INSERT INTO USERS (ID, NAME, EMAIL, PASSWORD, ROLE) VALUES ?";
-    var values = [
-        [uuid.v4(),'Fullstack12','fullstack@aaryan.ssj5.in','chaapu','manager']
-    ];
-    con.query(sql,[values],function(err,result){
+});
+
+
+router.get('/',function(req,res,next){
+    con.query('SELECT * FROM USERS',function(err,results){
         if(err)throw err;
-        console.log("Done");
+        console.log(results);
+        res.end(JSON.stringify(results));
     });
 });
+router.get('/:id',function(req,res){
+    var id = req.params.id;
+    var sql = "SELECT * FROM USERS WHERE ID = ?";
+    con.query(sql,[id],function(err,result){
+        if(err)throw err;
+        res.end(JSON.stringify(result));
+    });
+});
+router.post('/insert',function(req,res){
+    var sql = "INSERT INTO USERS (ID, NAME, EMAIL, PASSWORD, ROLE) VALUES ?";
+    var values = [
+        [uuid.v4(),req.body.name,req.body.email,req.body.password,req.body.role]
+    ];
+    con.query(sql,[values],function(err,result, fields){
+        if(err)throw err;
+        console.log("Done");
+        res.end(JSON.stringify(result));
+    });
+});
+module.exports = router;
